c75d3f89a1905c0824348f28816f9cd60752f3b1
Umut Saw <umut1tr@hotmail.com>
2024-12-17 15:02:52 +0100

    Add Connection String

--- Program.cs
+++ Program.cs
@@ -1,4 +1,13 @@
+using DevSpot.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// DBContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
